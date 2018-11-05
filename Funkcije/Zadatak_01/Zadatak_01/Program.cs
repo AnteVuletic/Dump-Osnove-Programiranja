31e607e2cdcc8c30e240e32642e8c75d34b03e4a
AnteVuletic <antevuletic1995@gmail.com>
2018-11-05 15:02:25 +0100

    1-2 Funkcije

---
+++ Funkcije/Zadatak_01/Zadatak_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_01
+{
+    class Program
+    {
+        static int Povecaj(int n)
+        {
+            return n + 10;
+        }
+        static void Main(string[] args)
+        {
+            int unesenBroj = 0;
+            Console.WriteLine("Unesite jedan broj:");
+            unesenBroj = int.Parse(Console.ReadLine());
+            Console.WriteLine(Povecaj(unesenBroj));
+        }
+    }
+}
---
+++ Funkcije/Zadatak_02/Zadatak_02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_02
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int unesenBrojJedan, unesenBrojDva = 0;
+            List<int> allDevidable = new List<int>();
+            Console.WriteLine("Unesite prvi broj:");
+            unesenBrojJedan = int.Parse(Console.ReadLine());
+            Console.WriteLine("Unesite drugi broj:");
+            unesenBrojDva = int.Parse(Console.ReadLine());
+            allDevidable = SviDjeljivi(unesenBrojJedan, unesenBrojDva);
+            Console.WriteLine("Svi djeljivi su: ");
+            for(int i = 0; i < allDevidable.Count; i++)
+            {
+                Console.Write(" " + allDevidable[i] + " ");
+            }
+            Console.WriteLine();
+        }  
+        static bool JeliDjeljiv(int prvi, int drugi)
+        {
+            if (prvi % drugi == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static List<int> SviDjeljivi(int prvi, int drugi)
+        {
+            List<int> allNummbersDevidable = new List<int>();
+            for(int i = 1; i <= prvi; i++)
+            {
+                if(JeliDjeljiv(drugi,i))
+                {
+                    allNummbersDevidable.Add(i);
+                }
+            }
+            return allNummbersDevidable;
+        }
+
+    }
+}
